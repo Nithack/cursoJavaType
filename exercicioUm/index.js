8655ea6b0ffe7d7480db34c67d51b592d3315215
Nithack <andreynithack@gmail.com>
2020-11-13 00:21:49 -0300

    first commit

---
+++ exercicioUm/index.js
@@ -0,0 +1,6 @@
+const num1 = Number(prompt('Digite o primeiro numero para soma'))
+const num2 = Number(prompt('Digite o segundo numero para soma'))
+const resultado = num1 + num2
+alert('O resultado da soma é ' + resultado)
+
+console.log('by Nit')
