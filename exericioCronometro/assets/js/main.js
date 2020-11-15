861f010a0500c89d99d30f6a536eeb0ca8a0ba25
Nithack <andreynithack@gmail.com>
2020-11-15 03:20:22 -0300

    Update exercicioCronometro

--- exericioCronometro/assets/js/main.js
+++ exericioCronometro/assets/js/main.js
@@ -1,41 +1,51 @@
-const cronometro = document.querySelector('.cronometro')
-const start = document.querySelector('.start')
-const pause = document.querySelector('.pause')
-const reset = document.querySelector('.reset')
-let seconds = 0
-let timer;
+const cronometro = () => {
 
-const setTimer = (seconds) => {
-    const timer = new Date(seconds * 1000)
-    return timer.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
+    const cronometro = document.querySelector('.cronometro')
+    const start = document.querySelector('.start')
+    const reset = document.querySelector('.reset')
+    let pause = false
+    let seconds = 0
+    let timer;
+
+    const setTimer = (seconds) => {
+        const timer = new Date(seconds * 1000)
+        return timer.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        })
+    }
+    const startTimer = () => {
+        clearInterval(timer)
+        if (pause) {
+            cronometro.classList.add('pauseCronometro')
+            start.innerHTML = 'start'
+            pause = false
+        } else {
+            cronometro.classList.remove('pauseCronometro')
+            start.innerHTML = 'pause'
+            pause = true
+            timer = setInterval(() => {
+                seconds++;
+                cronometro.innerHTML = setTimer(seconds)
+            }, 1000)
+        }
+    }
+    const resetTime = () => {
+        clearInterval(timer)
+        cronometro.classList.remove('pauseCronometro')
+        seconds = 0
+        start.innerHTML = 'start'
+        pause = false
+        cronometro.innerHTML = setTimer(0)
+    }
+
+    start.addEventListener('click', function() {
+        startTimer()
+
+    })
+
+    reset.addEventListener('click', () => {
+        resetTime()
     })
 }
-const startTimer = () => {
-    clearInterval(timer)
-    timer = setInterval(() => {
-        seconds++;
-        cronometro.innerHTML = setTimer(seconds)
-    }, 1000)
-}
-
-const pauseTimer = () => {
-    clearInterval(timer)
-}
-
-const resetTime = () => {
-    clearInterval(timer)
-    seconds = 0
-    cronometro.innerHTML = setTimer(0)
-}
-
-start.addEventListener('click', function() {
-    startTimer()
-})
-pause.addEventListener('click', () => {
-    pauseTimer()
-})
-reset.addEventListener('click', () => {
-    resetTime()
-})
+cronometro()
